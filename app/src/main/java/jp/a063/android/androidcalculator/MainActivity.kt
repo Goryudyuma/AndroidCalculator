0ea15eed8c3c8a4988010e8b84dbebb2196e0b7f
Goryudyuma <umaretekyoumade@gmail.com>
2017-04-11 13:06:23 +0900

    empty application

---
+++ app/src/main/java/jp/a063/android/androidcalculator/MainActivity.kt
@@ -0,0 +1,14 @@
+package jp.a063.android.androidcalculator
+
+import android.app.Activity
+import android.os.Bundle
+
+/**
+ * Created by umare on 2017/04/11.
+ */
+class MainActivity :Activity(){
+    override fun onCreate(savedInstanceState : Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
