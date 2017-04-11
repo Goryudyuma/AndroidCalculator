0954d9d6b6a4f2cdca104783bf64d0b551a9f790
Goryudyuma <umaretekyoumade@gmail.com>
2017-04-11 15:19:25 +0900

    add button and textbox

--- app/src/main/java/jp/a063/android/androidcalculator/MainActivity.kt
+++ app/src/main/java/jp/a063/android/androidcalculator/MainActivity.kt
@@ -1,7 +1,16 @@
 package jp.a063.android.androidcalculator
 
 import android.app.Activity
+import android.content.DialogInterface
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.Toast
+import android.widget.LinearLayout
+
+
+
+
 
 /**
  * Created by umare on 2017/04/11.
@@ -10,5 +19,7 @@ class MainActivity :Activity(){
     override fun onCreate(savedInstanceState : Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+
     }
 }
