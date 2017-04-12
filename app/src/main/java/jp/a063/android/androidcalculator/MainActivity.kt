2c59f0987717d08a9409e327925ad8b7cc8c14ba
Goryudyuma <umaretekyoumade@gmail.com>
2017-04-12 19:30:37 +0900

    create onClickListener for button

--- app/src/main/java/jp/a063/android/androidcalculator/MainActivity.kt
+++ app/src/main/java/jp/a063/android/androidcalculator/MainActivity.kt
@@ -1,25 +1,28 @@
 package jp.a063.android.androidcalculator
 
 import android.app.Activity
-import android.content.DialogInterface
 import android.os.Bundle
-import android.view.View
-import android.widget.Button
-import android.widget.Toast
-import android.widget.LinearLayout
-
-
-
-
+import kotlinx.android.synthetic.main.activity_main.*
 
 /**
- * Created by umare on 2017/04/11.
+ * Created by Goryudyuma on 2017/04/11.
  */
-class MainActivity :Activity(){
-    override fun onCreate(savedInstanceState : Bundle?) {
+class MainActivity : Activity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-
+        button1.setOnClickListener {
+            textView.text = "1"
+        }
+        button2.setOnClickListener {
+            textView.text = "2"
+        }
+        button3.setOnClickListener {
+            textView.text = "3"
+        }
+        button4.setOnClickListener {
+            textView.text = "4"
+        }
     }
 }
