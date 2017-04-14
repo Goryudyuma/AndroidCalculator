24dd5363c7af6234edd0eae27a5cc5ddc25f7ff8
Goryudyuma <umaretekyoumade@gmail.com>
2017-04-14 11:21:33 +0900

    fix int -> biginteger

--- app/src/main/java/jp/a063/android/androidcalculator/MainActivity.kt
+++ app/src/main/java/jp/a063/android/androidcalculator/MainActivity.kt
@@ -14,18 +14,50 @@ class MainActivity : Activity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        textView.text = cs.num(0)
 
+        button0.setOnClickListener {
+            textView.text = cs.num(0)
+        }
+        button00.setOnClickListener {
+            cs.num(0)
+            textView.text = cs.num(0)
+        }
         button1.setOnClickListener {
-            textView.text = cs.num(1).toString()
+            textView.text = cs.num(1)
         }
         button2.setOnClickListener {
-            textView.text = cs.num(2).toString()
+            textView.text = cs.num(2)
         }
         button3.setOnClickListener {
-            textView.text = cs.num(3).toString()
+            textView.text = cs.num(3)
         }
         button4.setOnClickListener {
-            textView.text = cs.num(4).toString()
+            textView.text = cs.num(4)
+        }
+        button5.setOnClickListener {
+            textView.text = cs.num(5)
+        }
+        button6.setOnClickListener {
+            textView.text = cs.num(6)
+        }
+        button7.setOnClickListener {
+            textView.text = cs.num(7)
+        }
+        button8.setOnClickListener {
+            textView.text = cs.num(8)
+        }
+        button9.setOnClickListener {
+            textView.text = cs.num(9)
+        }
+        buttonClear.setOnClickListener {
+            textView.text = cs.clear()
+        }
+        buttonPlus.setOnClickListener {
+            textView.text = cs.plus()
+        }
+        buttonEqual.setOnClickListener {
+            textView.text = cs.equal()
         }
     }
 }
--- app/src/main/java/jp/a063/android/androidcalculator/Service/CalculatorService/CalculatorService.kt
+++ app/src/main/java/jp/a063/android/androidcalculator/Service/CalculatorService/CalculatorService.kt
@@ -1,13 +1,42 @@
 package jp.a063.android.androidcalculator.Service.CalculatorService
 
+import java.math.BigInteger
+
 /**
  * Created by umare on 2017/04/13.
  */
 
 class CalculatorService {
-    private var now = 0
-    public fun num(n: Int): Int {
-        now += n
-        return now
+    private var now = BigInteger.valueOf(0)
+    private var memo = BigInteger.valueOf(0)
+    private var operator = '='
+
+    public fun num(n: Long): String {
+        memo = memo.multiply(BigInteger.valueOf(10)).plus(BigInteger.valueOf(n))
+        return memo.toString()
+    }
+
+    public fun equal(): String {
+        if (operator == '+') {
+            now = now.plus(memo)
+            memo = now
+            return now.toString()
+        }
+        return "error"
+    }
+
+    public fun plus(): String {
+        this.equal()
+        operator = '+'
+        now = memo
+        memo = BigInteger.valueOf(0)
+        return now.toString()
+    }
+
+    public fun clear(): String {
+        now = BigInteger.valueOf(0)
+        memo = BigInteger.valueOf(0)
+        operator = '='
+        return now.toString()
     }
 }
